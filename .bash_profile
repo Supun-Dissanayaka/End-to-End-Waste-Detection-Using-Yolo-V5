67cfc42a60fe70ffd375dbb68b6e180c3dabfaee
Supun Lakshan <lakshans793@gmail.com>
2024-04-08 23:12:02 +0530

    requirements added

---
+++ .bash_profile
@@ -0,0 +1,5 @@
+# Initialize Conda
+. /c/Users/laksh/anaconda3/etc/profile.d/conda.sh
+
+# Activate Conda environment (replace "base" with your desired environment name)
+conda activate base
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Supun Lakshan',
+    author_email= 'lakshans793@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
